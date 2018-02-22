c8a4343f3dc5c881828c5d1c3625553d382ad8d9
Minno Dang <minh.ngocdang@digital.cabinet-office.gov.uk>
2018-02-22 11:29:10 +0000

    EID-384: Setting the object name on s3 to be metadata's encoded url
    Pair: @adrianw1832 @minhngocd

--- upload_to_s3.rb
+++ upload_to_s3.rb
@@ -2,6 +2,7 @@
 
 require 'aws-sdk-s3'
 require 'yaml'
+require 'cgi'
 
 aws_region = 'eu-west-1'
 bucket_name = 'govukverify-eidas-metadata-aggregator-dev'
@@ -14,7 +15,7 @@ abort("Error: File supplied is not valid YAML") unless metadata_list
 s3 = Aws::S3::Resource.new(region: aws_region)
 
 metadata_list.each do |country, url|
-  obj = s3.bucket(bucket_name).object(country)
+  obj = s3.bucket(bucket_name).object(CGI.escape url)
   metadata = `curl -A "Mozilla" --max-time 15 #{url}`
   obj.put(body: metadata, server_side_encryption: encryption_algorithm, acl: 'public-read')
 end
