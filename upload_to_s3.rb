75225520281ec27dee952afaac5b821656519d7d
Minno Dang <minh.ngocdang@digital.cabinet-office.gov.uk>
2018-05-14 12:49:14 +0100

    EID-628: Trust Croatia's TLS CA cert for integration environment
    - Also removed uploading CGI escaped keys to s3 since we should only
      need to support hex now
    
    Co-authored-by: Hoque Ali <hoque.ali@digital.cabinet-office.gov.uk>

--- upload_to_s3.rb
+++ upload_to_s3.rb
@@ -6,7 +6,6 @@ require 'cgi'
 
 aws_region = ENV['AWS_REGION']
 bucket_name = ENV['BUCKET_NAME']
-encryption_algorithm = 'AES256'
 metadata_list_file = ENV['METADATA_FILE']
 
 abort('Please supply AWS_REGION, BUCKET_NAME, METADATA_FILE as environment variables') unless aws_region && bucket_name && metadata_list_file
@@ -18,24 +17,28 @@ puts "*"*50
 metadata_list = YAML.load_file File.join(__dir__, metadata_list_file)
 abort("Error: File supplied is not valid YAML") unless metadata_list
 
+def metadata_content url, tls_cert
+  optional_tls_cert = "--cacert #{tls_cert}" if tls_cert
+  `curl #{optional_tls_cert} -A "Mozilla" --max-time 15 #{url}`
+end
+
+def aws_put object, url, tls_cert
+  object.put(
+    body: metadata_content(url, tls_cert),
+    server_side_encryption: 'AES256',
+    acl: 'public-read',
+    content_type: 'application/samlmetadata+xml'
+  )
+end
+
 s3 = Aws::S3::Resource.new(region: aws_region)
 updated_metadata = []
 
-metadata_list.each do |country, url|
-  # Convert to Hex
+metadata_list.each do |country, metadata|
+  url = metadata['url']
   s3_object_key = url.unpack('H*')[0].downcase
   updated_metadata << s3_object_key
-  obj = s3.bucket(bucket_name).object(s3_object_key)
-  metadata = `curl -A "Mozilla" --max-time 15 #{url}`
-  obj.put(body: metadata, server_side_encryption: encryption_algorithm, acl: 'public-read', content_type: 'application/samlmetadata+xml')
-end
-
-metadata_list.each do |country, url|
-  s3_object_key = CGI.escape url
-  updated_metadata << s3_object_key
-  obj = s3.bucket(bucket_name).object(s3_object_key)
-  metadata = `curl -A "Mozilla" --max-time 15 #{url}`
-  obj.put(body: metadata, server_side_encryption: encryption_algorithm, acl: 'public-read', content_type: 'application/samlmetadata+xml')
+  aws_put s3.bucket(bucket_name).object(s3_object_key), url, metadata['tls_cert']
 end
 
 s3.bucket(bucket_name).objects.each do |object|
