8cdd384d24fddc9205a9900410ca56d63e33fa29
Minno Dang <minh.ngocdang@digital.cabinet-office.gov.uk>
2018-08-13 11:36:14 +0100

    EID-825: Added Dropwizard Application structure
    Added Module, Application, Configuration - all base skeleton waiting to
    be filled
    
    Co-authored-by: Simon Worthington <simon.worthington@digital.cabinet-office.gov.uk>

---
+++ src/main/java/uk/gov/ida/metadataaggregator/MetadataAggregatorModule.java
@@ -0,0 +1,11 @@
+package uk.gov.ida.metadataaggregator;
+
+import com.google.inject.Binder;
+import com.google.inject.Module;
+
+public class MetadataAggregatorModule implements Module {
+    @Override
+    public void configure(Binder binder) {
+
+    }
+}
---
+++ src/main/java/uk/gov/ida/metadataaggregator/config/MetadataAggregatorConfiguration.java
@@ -0,0 +1,6 @@
+package uk.gov.ida.metadataaggregator.config;
+
+import io.dropwizard.Configuration;
+
+public class MetadataAggregatorConfiguration extends Configuration {
+}
