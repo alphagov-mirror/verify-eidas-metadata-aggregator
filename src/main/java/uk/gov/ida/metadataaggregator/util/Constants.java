462eadeb2d79b90f91d39d28a064746464f36795
Felix Millne <felix.millne@digital.cabinet-office.gov.uk>
2018-09-07 16:11:07 +0100

    EID-887: Remove unused constants
    -Some values leftover in rebase that should be removed

--- src/main/java/uk/gov/ida/metadataaggregator/util/Constants.java
+++ src/main/java/uk/gov/ida/metadataaggregator/util/Constants.java
@@ -2,12 +2,4 @@ package uk.gov.ida.metadataaggregator.util;
 
 public class Constants {
     public static final String AGGREGATOR_CONFIG_FILE_NAME = "MetadataSourceConfiguration.json";
-    public static final String AWS_ACCESS_KEY = "AWS_ACCESS_KEY";
-    public static final String AWS_SECRET_KEY = "AWS_SECRET_KEY";
-    public static final String BUCKET_NAME = "METADATA_BUCKET";
-    public static final int SUCCESS_STATUS_CODE = 200;
-    public static final int SERVER_ERROR_STATUS_CODE = 500;
-    public static final String TRUST_ANCHOR_PASSCODE = "TRUST_ANCHOR_PASSCODE";
-    public static final String TRUST_ANCHOR_URI = "TRUST_ANCHOR_URI";
-    public static final String ENVIRONMENT_KEY = "ENVIRONMENT";
 }
