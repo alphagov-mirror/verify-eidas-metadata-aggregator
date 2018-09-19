3aaa037d7ec03e7e83e5d3c1882961a220db1cdd
Kevin Garwood <kevin.garwood@digital.cabinet-office.gov.uk>
2018-09-19 14:30:37 +0100

    EID-827: Express URLs without Hex encoding
    Authors: @kgarwood, @felixmarcusmillne

--- src/main/java/uk/gov/ida/metadataaggregator/healthcheck/ReconciliationHealthCheck.java
+++ src/main/java/uk/gov/ida/metadataaggregator/healthcheck/ReconciliationHealthCheck.java
@@ -1,6 +1,9 @@
 package uk.gov.ida.metadataaggregator.healthcheck;
 
 import com.codahale.metrics.health.HealthCheck;
+import org.apache.commons.codec.DecoderException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import uk.gov.ida.metadataaggregator.configuration.MetadataSourceConfiguration;
 import uk.gov.ida.metadataaggregator.core.S3BucketMetadataStore;
 import uk.gov.ida.metadataaggregator.exceptions.MetadataStoreException;
@@ -17,6 +20,7 @@ public class ReconciliationHealthCheck extends HealthCheck {
 
     private final S3BucketMetadataStore metadataStore;
     private final MetadataSourceConfiguration config;
+    private final Logger logger = LoggerFactory.getLogger(ReconciliationHealthCheck.class);
 
     @Inject
     public ReconciliationHealthCheck(S3BucketMetadataStore metadataStore, MetadataSourceConfiguration config) {
@@ -27,13 +31,39 @@ public class ReconciliationHealthCheck extends HealthCheck {
     @Override
     public Result check() throws MetadataStoreException {
         Set<String> fileUrlsInBucket = new HashSet<>(metadataStore.getAllHexEncodedUrlsFromS3Bucket());
-        Set<String> inBucketNotInConfig = new HashSet<>(fileUrlsInBucket);
 
-        Set<String> filesUrlsInConfig = getHexEncodeConfigUrls();
-        Set<String> inConfigNotInBucket = new HashSet<>(filesUrlsInConfig);
+        Set<String> fileUrlsInConfig = getHexEncodeConfigUrls();
 
-        inConfigNotInBucket.removeAll(fileUrlsInBucket);
-        inBucketNotInConfig.removeAll(filesUrlsInConfig);
+        Set<String> inConfigNotInBucket = new HashSet<>();
+        Set<String> invalidHexEncodedUrl = new HashSet<>();
+
+        for (String hexEncodedUrl : fileUrlsInConfig) {
+            if (!fileUrlsInBucket.contains(hexEncodedUrl)) {
+                String decodeString;
+                try {
+                    decodeString = HexUtils.decodeString(hexEncodedUrl);
+                    inConfigNotInBucket.add(decodeString);
+                } catch (DecoderException e) {
+                    logger.error("Unable to decode string {} ", hexEncodedUrl, e);
+                    invalidHexEncodedUrl.add(hexEncodedUrl);
+                }
+            }
+        }
+
+        Set<String> inBucketNotInConfig = new HashSet<>();
+
+        for (String hexEncodedUrl : fileUrlsInBucket) {
+            if (!fileUrlsInConfig.contains(hexEncodedUrl)) {
+                String decodeString;
+                try {
+                    decodeString = HexUtils.decodeString(hexEncodedUrl);
+                    inBucketNotInConfig.add(decodeString);
+                } catch (DecoderException e) {
+                    logger.error("Unable to decode string {} ", hexEncodedUrl, e);
+                    invalidHexEncodedUrl.add(hexEncodedUrl);
+                }
+            }
+        }
 
         if (inConfigNotInBucket.isEmpty() && inBucketNotInConfig.isEmpty()) {
             return Result.healthy();
@@ -41,6 +71,7 @@ public class ReconciliationHealthCheck extends HealthCheck {
             return Result.builder().unhealthy()
                     .withDetail("inConfigNotInBucket", inConfigNotInBucket)
                     .withDetail("inBucketNotInConfig", inBucketNotInConfig)
+                    .withDetail("invalidHexEncodedUrl", invalidHexEncodedUrl)
                     .build();
         }
     }
