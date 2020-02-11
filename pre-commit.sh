42d3a44f614a53029ecaa642ad3b33cc35ced0d1
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2020-02-11 11:14:11 +0000

    Delete all the code
    Metadata aggregation is now done in the concourse pipeline and does
    not depend on the code in this repo.  The only thing we still need is
    the JSON files in src/main/resources.

--- pre-commit.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-./gradlew --daemon --parallel clean build test
--- src/main/java/uk/gov/ida/metadataaggregator/exceptions/EnvironmentVariableException.java
+++
@@ -1,7 +0,0 @@
-package uk.gov.ida.metadataaggregator.exceptions;
-
-public class EnvironmentVariableException extends Exception {
-    public EnvironmentVariableException(String message) {
-        super(message);
-    }
-}
